de561d38887a4d6b0f717416aa89a9f5dbdfade8
LocB2014669 <locb2014669@student.ctu.edu.vn>
2024-02-20 19:17:36 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const contactsRouter = require("./app/routes/contact.route");
+app.use("/api/contacts", contactsRouter);
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the contact book application." });
 });
