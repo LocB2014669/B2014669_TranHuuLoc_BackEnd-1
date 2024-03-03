651871432b3ce90ef72427bbeccfabfe811b6efb
LocB2014669 <locb2014669@student.ctu.edu.vn>
2024-03-03 10:07:40 +0700

    Ket noi thanh cong databass

--- app/config/index.js
+++ app/config/index.js
@@ -1,6 +1,9 @@
 const config = {
   app: {
     port: process.env.PORT || 3000,
+    db: {
+      uri: process.env.MONGODB_URI || "mongodb+srv://ct447:muctu17121911@cluster0.graoxzd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
+    },
   },
 };
 
